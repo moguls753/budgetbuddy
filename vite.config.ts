6207827ecec60a30b907ac0e3e8c32c958e5616a
Eike Rackwitz <eike.rackwitz@mail.de>
2026-02-05 21:03:37 +0100

    vite-ruby takes care

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    react(),
   ],
 })
